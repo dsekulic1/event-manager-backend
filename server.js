c715c16114bb22527af8a4ac012ae0497a29887f
unknown <dsekulic1@etf.unsa.ba>
2021-11-05 16:05:32 +0100

    revert merge

--- server.js
+++ server.js
@@ -1,53 +1,20 @@
-require('dotenv').config()
-require('express-async-errors')
-//express
-const express = require('express')
-const app = express()
-
-// rest of the packages
-const cookieParser = require('cookie-parser')
-const helmet = require('helmet')
-const xss = require('xss-clean')
-const cors = require('cors')
-const mongoSanitize = require('express-mongo-sanitize')
-const fileUpload = require('express-fileupload')
-const rateLimiter = require('express-rate-limit')
-
-//db
 const connectDB = require('./db/connect')
-
-//routers
-const authRouter = require('./routes/authRoutes')
-const userRouter = require('./routes/userRoutes')
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const tasks = require('./routes/tasks')
 const slack = require('./routes/slack')
 const email = require('./routes/email')
-const tasks = require('./routes/tasks')
-
-//middleware
+require('dotenv').config()
 const notFound = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
-app.set('trust proxy', 1)
-app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000,
-    max: 60,
-  })
-)
-app.use(helmet())
-app.use(cors())
-app.use(xss())
-app.use(mongoSanitize())
-
-app.use(express.json())
-app.use(cookieParser(process.env.JWT_SECRET))
-
+//middleware
 app.use(express.static('./public'))
-app.use(fileUpload())
 app.use(express.json())
-
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/users', userRouter)
+app.use(cors())
+app.options('*', cors())
+//routes
 app.use('/api/v1/tasks', tasks)
 app.use('/api/v1/slack', slack)
 app.use('/api/v1/email', email)
